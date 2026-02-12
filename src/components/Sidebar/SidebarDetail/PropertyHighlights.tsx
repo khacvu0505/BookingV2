5d85982d17dda8dd10b64a9a3ba18e018dc858a6
Vu Nguyen <khacvu0505@gmail.com>
2026-02-12 22:38:42 +0700

    feat: Update UI

--- src/components/Sidebar/SidebarDetail/PropertyHighlights.tsx
+++ src/components/Sidebar/SidebarDetail/PropertyHighlights.tsx
@@ -1,5 +1,6 @@
 import { groupBy } from "@/utils/utils";
 import { useEffect, useState } from "react";
+import "./PropertyHighlights.style.scss";
 
 interface PropertyHighlightsProps {
   amenities: any[];
@@ -18,34 +19,26 @@ const PropertyHighlights = ({ amenities }: PropertyHighlightsProps) => {
     return;
   }
   return (
-    <div>
-      <div className="row">
-        {amenitiesByGroup &&
-          Object.keys(amenitiesByGroup)?.map((item: string) => (
-            <div className="col-xl-4 mb-30" key={item}>
-              <div className="row y-gap-30">
-                <div className="col-12">
-                  <div>
-                    <div className="d-flex items-center text-16 lg:text-15 fw-500">
-                      <i
-                        className={`${amenitiesByGroup[item][0]?.icon} text-20 mr-10 text-primary-500`}
-                      />
-                      {item}
-                    </div>
-                    <ul className="text-15 lg:text-14 pt-10">
-                      {amenitiesByGroup[item].map((amen: any, index: number) => (
-                        <li className="d-flex items-center" key={index}>
-                          <i className={`${amen.icon} mr-5`} />
-                          {amen.text}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
+    <div className="property-highlights-masonry">
+      {amenitiesByGroup &&
+        Object.keys(amenitiesByGroup)?.map((item: string) => (
+          <div className="property-highlights-masonry__item" key={item}>
+            <div className="d-flex items-center text-18 lg:text-16 fw-500">
+              {/* <i
+                className={`${amenitiesByGroup[item][0]?.icon} text-20 mr-10 text-primary-500`}
+              /> */}
+              {item}
             </div>
-          ))}
-      </div>
+            <ul className="text-15 lg:text-14">
+              {amenitiesByGroup[item].map((amen: any, index: number) => (
+                <li className="d-flex items-end ml-10" key={index}>
+                  <i className={`${amen.icon} mr-5`} />
+                  {amen.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };

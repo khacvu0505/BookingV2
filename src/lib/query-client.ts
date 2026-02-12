da8c2812baa23caa5854dbe923846eea8cf6c2ed
Vu Nguyen <khacvu0505@gmail.com>
2026-02-12 11:29:10 +0700

    feat: Adjust time cache data called api

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -4,8 +4,8 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 5 * 60 * 1000, // 5 minutes
-        gcTime: 10 * 60 * 1000, // 10 minutes
+        staleTime: 2 * 60 * 1000, // 2 minutes
+        gcTime: 5 * 60 * 1000, // 5 minutes
         retry: 1,
         refetchOnWindowFocus: false,
       },
